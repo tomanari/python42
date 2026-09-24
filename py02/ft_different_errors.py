7b32210cc3873bde6b357087d2ad38ba908d6774
Marcella Tomanari <tomanari.lol@gmail.com>
2026-09-24 15:26:36 -0300

    Add files via upload

--- py02/ft_different_errors.py
+++ py02/ft_different_errors.py
@@ -1,30 +1,45 @@
 def garden_operations(operation_number: int) -> None:
-  i = 0
 
-  try:
-      if i >= 0 and i <= 4:
-          print(f"Input data is '{operation_number[i]}'")
-          print(f"Temperature is now '{result}ºC'\n")
-  except ValueError:
-      print(f"Input data is {operation_number[i]}")
-      print("Caught input_temperature error: invalid literal"
-            f"for int() with base 10: '{operation_number[i]}'\n")
-  except ZeroDivisionError:
-      print(f"Input data is {operation_number[i]}")
-      print("Caught input_temperature error: invalid literal"
-            f"for int() with base 10: '{operation_number[i]}'\n")
-  except FileNotFoundError:
-        print(f"Input data is {operation_number[i]}")
-        print("Caught input_temperature error: invalid literal"
-              f"for int() with base 10: '{operation_number[i]}'\n")
-  except TypeError:
-        print(f"Input data is {operation_number[i]}")
-        print("Caught input_temperature error: invalid literal"
-              f"for int() with base 10: '{operation_number[i]}'\n") 
-  print("All tests completed - program didn't crash!")
+    if operation_number == 0:
+        int("abc")
+    elif operation_number == 1:
+        10 / 0
+    elif operation_number == 2:
+        open("unknown.py")
+    elif operation_number == 3:
+        "42" + 0
+    else:
+        return
+
+
+def test_error_types():
+    i = 0
+
+    while i <= 4:
+        try:
+            garden_operations(i)
+            print("Testing operation 4...")
+            print("Operation completed successfully\n")
+        except ValueError:
+            print("Testing operation 0...")
+            print("Caught ValueError: invalid literal"
+                  "for int() with base 10: 'abc'")
+        except ZeroDivisionError:
+            print("Testing operation 1...")
+            print("Caught ZeroDivisionError: division by zero")
+        except FileNotFoundError:
+            print("Testing operation 2...")
+            print("Caught FileNotFoundError: [Errno 2] No such file"
+                  "or directory: '/non/existent/file'")
+        except TypeError:
+            print("Testing operation 3...")
+            print("Caught TypeError: can only concatenate str "
+                  "(not 'int') to str")
+        i += 1
+    print("All tests completed - program didn't crash!")
 
 
 if __name__ == '__main__':
     print("=== Garden Temperature Checker ===\n")
 
-    test_temperature()
+    test_error_types()
